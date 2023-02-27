fd57ed065a584ceac57731d61b1872c39c344e6e
OlgaOO7 <110050897+OlgaOO7@users.noreply.github.com>
2023-02-27 11:51:52 +0000

    Deploying to gh-pages from @ OlgaOO7/goit-js-hw-09@54d4ae026a522e82bc2df2739bcf2967f36ae63e 🚀

---
+++ 01-color-switcher.e83d2485.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),d=null;e.disabled=!0,t.addEventListener("click",(function(o){t.disabled=!0,e.disabled=!1,d=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(n){clearInterval(d),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.e83d2485.js.map
